d1cb36dcf34a9a4bd07e10b4aebb9aae2d8f5cb9
anhtuan251101 <s3818169@rmit.edu.vn>
2021-05-06 19:35:58 +0700

    Added overlay to About Us

--- js/jvscp.js
+++ js/jvscp.js
@@ -32,3 +32,45 @@ window.onload = () => {
     element.style.display = "none";
   }
 };
+
+// About Us Overlay
+
+document.getElementById("Quang").addEventListener("click", overlayOnQuang);
+
+function overlayOnQuang() {
+  document.getElementById("overlayQuang").style.display = "block";
+  }
+
+function overlayOffQuang() {
+  document.getElementById("overlayQuang").style.display = "none";
+  }
+
+document.getElementById("Chang").addEventListener("click", overlayOnChang);
+
+function overlayOnChang() {
+  document.getElementById("overlayChang").style.display = "block";
+  }
+
+function overlayOffChang() {
+  document.getElementById("overlayChang").style.display = "none";
+  }
+
+document.getElementById("Tuan").addEventListener("click", overlayOnTuan);
+
+function overlayOnTuan() {
+  document.getElementById("overlayTuan").style.display = "block";
+  }
+
+function overlayOffTuan() {
+  document.getElementById("overlayTuan").style.display = "none";
+  }
+
+document.getElementById("Minh").addEventListener("click", overlayOnMinh);
+
+function overlayOnMinh() {
+  document.getElementById("overlayMinh").style.display = "block";
+  }
+
+function overlayOffMinh() {
+  document.getElementById("overlayMinh").style.display = "none";
+  }
