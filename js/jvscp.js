0407ba3b0f83bba41a7f991dbdc8ce0489bd6313
Quang <s3817788@rmit.edu.vn>
2021-05-06 19:08:03 +0700

    Fixed Display

--- js/jvscp.js
+++ js/jvscp.js
@@ -5,7 +5,7 @@ window.onload = () => {
         <p>My website uses cookies necessary for its basic fucntioning. by continuing browsing, you consent to my
             use of cookies and other technology</p>
         <div><a class="btn-accept" id="button-accept" href="#">I Understand</a></div>
-        <div><a href="../WebASM1/Mall/Privacy Policy.html">Learn more</a></div>
+        <div><a href="https://www.cookiebot.com/en/cookie-policy/#:~:text=that%20you%20comply.-,What%20is%20a%20cookie%20policy%3F,world%20this%20data%20is%20sent.">Learn more</a></div>
     </div>`;
   var cookie_insert = document.createElement("div");
   cookie_insert.innerHTML = cookie;
@@ -13,27 +13,22 @@ window.onload = () => {
 
   var element = document.getElementById("consent-pop-up");
 
-  var t = setTimeout(openPopUp, 0);
-  function openPopUp() {
-    element = document.getElementById("consent-pop-up");
-    element.style.visibility = "visible";
-    element.style.opacity = "1";
+  const storageType = localStorage;
+  const consentPropertyName = "Accept Cookies";
+  const saveToStorage = function () {
+    return storageType.setItem(consentPropertyName, true);
   }
-    const storageType = localStorage;
-    const consentPropertyName = "sdasd";
-    const saveToStorage = () => storageType.setItem(consentPropertyName, true);
-
-    const acceptFn = (event) => {
-      saveToStorage(storageType);
-      consentPopup.classList.add("consent-pop-up");
-      consentPopup.style.display = "none";
-    };
-    const consentPopup = document.getElementById("consent-pop-up");
-    const acceptBtn = document.getElementById("button-accept");
-    acceptBtn.addEventListener("click", acceptFn);
-
-    if (storageType.getItem(consentPropertyName) == "true") {
-      consentPopup.classList.add("consent-pop-up");
-      consentPopup.style.display = "none";
-    }
+  
+  const acceptFn = (event) => {
+    saveToStorage();
+    element.classList.add("consent-pop-up");
+    element.style.display = "none";
   };
+  const acceptBtn = document.getElementById("button-accept");
+  acceptBtn.addEventListener("click", acceptFn);
+
+  if (storageType.getItem(consentPropertyName) == "true") {
+    element.classList.add("consent-pop-up");
+    element.style.display = "none";
+  }
+};
