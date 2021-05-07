f8f03082529aa206191638642e6a4ac99e3f750f
Quang <s3817788@rmit.edu.vn>
2021-05-07 23:08:15 +0700

    Finished Account

---
+++ js/Login.js
@@ -0,0 +1,33 @@
+if (localStorage.getItem('User') == null) {
+    var username = String;
+    var password = String;
+    document.getElementById("login-form").addEventListener("submit", function (ev) {
+        if (!validateLogin()) {
+            ev.preventDefault();
+            document.getElementById("error-message").style.display = "block";
+        }
+        else {
+            localStorage.setItem('User', username);
+            localStorage.setItem('Password', password);
+            setTimeout(goToMyA, 0);
+        }
+    })
+
+    function validateLogin() {
+        username = document.getElementById("username").value;
+        password = document.getElementById("password").value;
+        email_pattern = /^(([a-zA-Z0-9]+\.){1,}|([a-zA-Z0-9]+){2,})([[a-zA-Z0-9]+@)([a-zA-Z0-9]+\.)([a-zA-Z0-9]+\.)*([a-zA-Z]{2,5})$/
+        if (email_pattern.test(username) && (password == "password")) {
+            return true;
+        }
+        return false;
+    }
+
+    function goToMyA() {
+        window.location.href = "My Account.html";
+    }
+}
+else{
+    window.location.href = "My Account.html";
+    document.getElementById("error-message").style.display = "none";
+}
