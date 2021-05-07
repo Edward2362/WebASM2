94d9bbadb4530f7fae3c2a7b6f7e1f423528d61a
Quang <s3817788@rmit.edu.vn>
2021-05-07 16:15:06 +0700

    Updated js file

---
+++ js/About us.js
@@ -0,0 +1,39 @@
+document.getElementById("Quang").addEventListener("click", overlayOnQuang);
+
+function overlayOnQuang() {
+  document.getElementById("overlayQuang").style.display = "block";
+}
+
+function overlayOffQuang() {
+  document.getElementById("overlayQuang").style.display = "none";
+}
+
+document.getElementById("Chang").addEventListener("click", overlayOnChang);
+
+function overlayOnChang() {
+  document.getElementById("overlayChang").style.display = "block";
+}
+
+function overlayOffChang() {
+  document.getElementById("overlayChang").style.display = "none";
+}
+
+document.getElementById("Tuan").addEventListener("click", overlayOnTuan);
+
+function overlayOnTuan() {
+  document.getElementById("overlayTuan").style.display = "block";
+}
+
+function overlayOffTuan() {
+  document.getElementById("overlayTuan").style.display = "none";
+}
+
+document.getElementById("Minh").addEventListener("click", overlayOnMinh);
+
+function overlayOnMinh() {
+  document.getElementById("overlayMinh").style.display = "block";
+}
+
+function overlayOffMinh() {
+  document.getElementById("overlayMinh").style.display = "none";
+}
